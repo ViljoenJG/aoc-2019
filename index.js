48d127ad544d7e743b22da5f77d0769db534dd11
Kobus Viljoen <kobus@colonyhq.com>
2019-12-02 10:20:02 +0200

    Refactored to use single index file for all days

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const fs = require("fs");
 const path = require("path");
 
+const dayArg = process.argv[2] || "1";
+const day = dayArg.padStart(2, "0");
+
 const input = fs
-  .readFileSync(path.join(__dirname, "input.txt"), {
+  .readFileSync(path.join(__dirname, `day${day}`, "input.txt"), {
     encoding: "utf-8"
   })
   .trim();
 
-const { part1, part2 } = require("./solution");
+const { part1, part2 } = require(`./day${day}/solution`);
 
 const solution1 = part1(input);
 console.log(solution1);
